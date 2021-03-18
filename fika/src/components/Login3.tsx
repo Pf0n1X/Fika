98ae0b93a5f3c05d2e17a7442f7d286ea76f2c5a
Tomer Erusalimsky <bboypsyfly@gmail.com>
2021-03-18 07:08:41 +0200

    Bla

---
+++ fika/src/components/Login3.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import '../styles/login3.css';
+import { Link } from 'react-router-dom';
+
+const Login3 = () => {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="2000" height="500" viewBox="0 0 2400 707">
+        <defs>
+    <pattern id="pattern" width="1" height="1" viewBox="-10.06 -10.06 68.121 68.121">
+    </pattern>
+    <pattern id="pattern-2" preserveAspectRatio="none" width="100%" height="100%" viewBox="0 0 48 48">
+    </pattern>
+    <pattern id="pattern-3" preserveAspectRatio="none" width="100%" height="100%" viewBox="0 0 48 48">
+    </pattern>
+    <pattern id="pattern-4" width="1" height="1" viewBox="-2 1 38 38">
+    </pattern>
+    <clipPath id="clip-login3">
+      <rect width="913" height="707"/>
+    </clipPath>
+  </defs>
+  <g id="login3" className="cls-1">
+    <text id="_באיזו_פלטפורמה" data-name="? באיזו פלטפורמה" className="cls-2" transform="translate(23)"><tspan x="-247.625" y="71">? </tspan><tspan y="71">באיזו פלטפורמה</tspan></text>
+    <g id="ButtonNext" transform="translate(-156.711 233)">
+      <g id="Rectangle_1" data-name="Rectangle 1" className="cls-3" transform="translate(502.711 378)">
+        <rect className="cls-11" width="187" height="54" rx="27"/>
+        <rect className="cls-12" x="1.5" y="1.5" width="184" height="51" rx="25.5"/>
+      </g>
+      <text id="המשך" className="cls-4" transform="translate(583.711 414)"><tspan x="-40.915" y="0"><Link to="/">סיום</Link></tspan></text>
+      <rect id="iconfinder_right_4829872_1_" data-name="iconfinder_right_4829872 (1)" className="cls-5" width="48" height="48" transform="translate(627.711 381)"/>
+    </g>
+    <g id="ProgressBar3" transform="translate(-472 -195)">
+      <line id="Line_1" data-name="Line 1" className="cls-6" x2="198" transform="translate(810.5 765.5)"/>
+      <g id="Ellipse_1" data-name="Ellipse 1" className="cls-7" transform="translate(808 758)">
+        <ellipse className="cls-11" cx="8" cy="7.5" rx="8" ry="7.5"/>
+        <ellipse className="cls-12" cx="8" cy="7.5" rx="7.5" ry="7"/>
+      </g>
+      <g id="Ellipse_3" data-name="Ellipse 3" className="cls-7" transform="translate(904 758)">
+        <ellipse className="cls-11" cx="8" cy="7.5" rx="8" ry="7.5"/>
+        <ellipse className="cls-12" cx="8" cy="7.5" rx="7.5" ry="7"/>
+      </g>
+      <g id="Ellipse_4" data-name="Ellipse 4" className="cls-7" transform="translate(1000 758)">
+        <ellipse className="cls-11" cx="8" cy="7.5" rx="8" ry="7.5"/>
+        <ellipse className="cls-12" cx="8" cy="7.5" rx="7.5" ry="7"/>
+      </g>
+    </g>
+    <g id="ButtonSingIn" transform="translate(-461.711 -184)">
+      <g id="Rectangle_1-2" data-name="Rectangle 1" className="cls-3" transform="translate(502.711 378)">
+        <rect className="cls-11" width="195" height="54" rx="27"/>
+        <rect className="cls-12" x="1.5" y="1.5" width="192" height="51" rx="25.5"/>
+      </g>
+      <text id="ספרים" className="cls-4" transform="translate(586.711 417)"><tspan x="-42.63" y="0">ספרים</tspan></text>
+    </g>
+    <g id="ButtonSingIn-2" data-name="ButtonSingIn" transform="translate(-501 -184)">
+      <g id="ButtonSingIn-3" data-name="ButtonSingIn" transform="translate(254.289)">
+        <g id="Rectangle_1-3" data-name="Rectangle 1" className="cls-3" transform="translate(502.711 378)">
+          <rect className="cls-11" width="195" height="54" rx="27"/>
+          <rect className="cls-12" x="1.5" y="1.5" width="192" height="51" rx="25.5"/>
+        </g>
+        <text id="כתבות" className="cls-4" transform="translate(586.711 417)"><tspan x="-43.54" y="0">כתבות</tspan></text>
+      </g>
+      <rect id="outline_book_online_black_48dp" className="cls-8" width="37" height="37" rx="18.5" transform="translate(896 387)"/>
+    </g>
+    <g id="ButtonSingIn-4" data-name="ButtonSingIn" transform="translate(-501 -184)">
+      <g id="ButtonSingIn-5" data-name="ButtonSingIn" transform="translate(469.289)">
+        <g id="Rectangle_1-4" data-name="Rectangle 1" className="cls-3" transform="translate(502.711 378)">
+          <rect className="cls-11" width="195" height="54" rx="27"/>
+          <rect className="cls-12" x="1.5" y="1.5" width="192" height="51" rx="25.5"/>
+        </g>
+        <text id="סרטונים" className="cls-4" transform="translate(586.711 417)"><tspan x="-53.76" y="0">סרטונים</tspan></text>
+      </g>
+      <rect id="outline_play_circle_black_48dp" className="cls-9" width="43" height="43" rx="21.5" transform="translate(1115 385)"/>
+    </g>
+    <g id="ButtonSingIn-6" data-name="ButtonSingIn" transform="translate(-501 -184)">
+      <g id="ButtonSingIn-7" data-name="ButtonSingIn" transform="translate(351.289 88)">
+        <g id="Rectangle_1-5" data-name="Rectangle 1" className="cls-3" transform="translate(502.711 378)">
+          <rect className="cls-11" width="242" height="54" rx="27"/>
+          <rect className="cls-12" x="1.5" y="1.5" width="239" height="51" rx="25.5"/>
+        </g>
+        <text id="פודקאסטים" className="cls-4" transform="translate(605.711 417)"><tspan x="-78.015" y="0">פודקאסטים</tspan></text>
+      </g>
+      <rect id="outline_graphic_eq_black_48dp" className="cls-10" width="38" height="38" transform="translate(1040 477)"/>
+    </g>
+    <g id="ButtonSingIn-8" data-name="ButtonSingIn" transform="translate(-501 -184)">
+      <g id="Group_14" data-name="Group 14" transform="translate(136.289 88)">
+        <g id="Rectangle_1-6" data-name="Rectangle 1" className="cls-3" transform="translate(502.711 378)">
+          <rect className="cls-11" width="195" height="54" rx="27"/>
+          <rect className="cls-12" x="1.5" y="1.5" width="192" height="51" rx="25.5"/>
+        </g>
+        <text id="סרטים" className="cls-4" transform="translate(586.711 417)"><tspan x="-43.015" y="0">סרטים</tspan></text>
+      </g>
+    </g>
+    <g id="ButtonSingIn-9" data-name="ButtonSingIn" transform="translate(-501 -184)">
+      <g id="Rectangle_1-7" data-name="Rectangle 1" className="cls-3" transform="translate(1187 378)">
+        <rect className="cls-11" width="195" height="54" rx="27"/>
+        <rect className="cls-12" x="1.5" y="1.5" width="192" height="51" rx="25.5"/>
+      </g>
+      <text id="מאמרים" className="cls-4" transform="translate(1271 417)"><tspan x="-54.827" y="0">מאמרים</tspan></text>
+    </g>
+    <g id="ButtonSingIn-10" data-name="ButtonSingIn" transform="translate(-501 -184)">
+      <text id="תרבות" className="cls-4" transform="translate(1191 505)"><tspan x="-43.05" y="0">תרבות</tspan></text>
+      <g id="Rectangle_1-8" data-name="Rectangle 1" className="cls-3" transform="translate(1111 467)">
+        <rect className="cls-11" width="195" height="54" rx="27"/>
+        <rect className="cls-12" x="1.5" y="1.5" width="192" height="51" rx="25.5"/>
+      </g>
+    </g>
+  </g>
+</svg>
+    );
+};
+
+export default Login3;
--- fika/src/routes/Chat.tsx
+++ fika/src/routes/Chat.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Login1 from '../components/Login3';
 
 const Chat = () => {
     return (
-        <div>
-            This is the chat route
-        </div>
+        // <div>
+        //     This is the chat route
+        // </div>
+        <Login1 />
     )
 };
 
